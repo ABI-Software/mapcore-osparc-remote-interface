b4965ae588a2066128753f19f82d0ddfd030da3d
alan-wu <alan.wu@auckland.ac.nz>
2019-09-19 16:23:34 +1200

    Make node modules external.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   mode: "none",
   entry: {
     "mapcore-osparc-remote-interface": "./src/index.js",
   },
+  externals: [
+    nodeExternals({}),
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: "[name].js",
